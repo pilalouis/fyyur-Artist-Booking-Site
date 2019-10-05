3bf3f605e6fef20304b6c2215cb90676aee2f964
Pila louis <plouisasobo@gmail.com>
2019-10-05 22:30:53 +0100

    form data inserted as a new venue record in the db

--- app.py
+++ app.py
@@ -243,12 +243,37 @@ def create_venue_form():
 def create_venue_submission():
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  form = VenueForm(request.form)
+  if form.validate():
+    try:
+      seeking_talent = False
+      seeking_description = ''
+      if 'seeking_talent' in request.form:
+        seeking_talent = request.form['seeking_talent'] == 'y'
+      if 'seeking_description' in request.form:
+        seeking_description = request.form['seeking_description']
+      new_venue = Venue(
+        name=request.form['name'],
+        genres=request.form.getlist('genres'),
+        address=request.form['address'],
+        city=request.form['city'],
+        state=request.form['state'],
+        phone=request.form['phone'],
+        website=request.form['website'],
+        facebook_link=request.form['facebook_link'],
+        image_link=request.form['image_link'],
+        seeking_talent=seeking_talent,
+        seeking_description=seeking_description,
+      )
+      #insert new venue records into the db
+      Venue.insert(new_venue)
+      # on successful db insert, flash success
+      flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    except SQLAlchemyError as e:
+      # TODO: on unsuccessful db insert, flash an error instead.
+      # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
+      # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+      flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
   return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
