7ca01cd237873794b9172576423b07a6919ca22c
Pila louis <plouisasobo@gmail.com>
2019-10-09 00:54:57 +0100

    minor modifications on model

--- models.py
+++ models.py
@@ -105,11 +105,56 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     shows = db.relationship('Show', backref='Artist', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+    def __init__(self, name, genres, city, state, phone, image_link, website, facebook_link,
+                 seeking_venue=False, seeking_description=""):
+        self.name = name
+        self.genres = genres
+        self.city = city
+        self.state = state
+        self.phone = phone
+        self.website = website
+        self.facebook_link = facebook_link
+        self.seeking_description = seeking_description
+        self.image_link = image_link
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def short(self):
+        return{
+            'id': self.id,
+            'name':self.name,
+        }
+    
+    def details(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'genres': self.genres,
+            'city': self.city,
+            'state':self.state,
+            'phone': self.phone,
+            'website': self.website,
+            'facebook_link': self.facebook_link,
+            'seeking_venue': self.seeking_venue,
+            'seeking_description': self.seeking_description,
+            'image_link': self.image_link,
+
+        }
+
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
 
 #show model
 class Show(db.Model):
